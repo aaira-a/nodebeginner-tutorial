6d724ce68ef37d4030440ef8bb34ae4f999a66a9
azam alias <azamalias@gmail.com>
2016-12-11 00:17:41 +0800

    used non-blocking operation in request handler, using sleep-async package

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,22 @@
 
-function start(){
+var sleep = require("sleep-async")();
+
+function start(response){
     console.log("Request handler 'start' was called.");
 
-    function sleep(miliSeconds){
-        var startTime = new Date().getTime();
-        while (new Date().getTime() < startTime + miliSeconds);
-    };
+    sleep.sleep(10000, function(){
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello Start");
+        response.end();       
+    });
 
-    sleep(10000);
-    return "Hello Start";
 };
 
-function upload(){
+function upload(response){
     console.log("Request handler 'upload' was called.");
-    return "Hello Upload";
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Upload");
+    response.end();
 };
 
 exports.start = start;

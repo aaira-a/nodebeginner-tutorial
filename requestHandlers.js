9ae098532284ec5a6dd2446f9c6348cd582f1c1e
azam alias <azamalias@gmail.com>
2016-12-11 00:45:51 +0800

    updated handlers to display html form and display post data

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,21 +1,32 @@
 
-var sleep = require("sleep-async")();
+var querystring = require("querystring");
 
-function start(response){
+function start(response, postData){
     console.log("Request handler 'start' was called.");
 
-    sleep.sleep(10000, function(){
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello Start");
-        response.end();       
-    });
+    var body = '<html>' +
+    '<head>' + 
+    '<meta http-equiv="Content-Type" content="text/html; ' +
+    'charset=UTF-8" />' +
+    '</head>' +
+    '<body>' +
+    '<form action="/upload" method="post">' +
+    '<textarea name="text" rows="20" cols="60"></textarea>' +
+    '<input type="submit" value="Submit text" />' +
+    '</form>' +
+    '</body>' +
+    '</html>' ;
 
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
 };
 
-function upload(response){
+function upload(response, postData){
     console.log("Request handler 'upload' was called.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello Upload");
+    response.write("You've sent the text: " +
+        querystring.parse(postData).text);
     response.end();
 };
 
